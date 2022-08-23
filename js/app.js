7237949deef984b09b319a2872a92fe64c40b0ab
ismail360deg <ismailbhuiyan2000@gmail.com>
2022-08-23 14:25:04 +0600

    budject js complete

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 const cartArray = [];
 
+
 function display(cartplayer) {
 
     console.log(cartplayer);
     const tableBody = document.getElementById("players-cart");
     tableBody.innerHTML = '';
 
-    for (let i = 0; i < cartplayer.length; i++) {
+
+
+    for (let i = 0; i < 5; i++) {
+
 
         // console.log(cartArray[i].playerName,);
         const name = cartArray[i].playerName;
@@ -14,21 +18,40 @@ function display(cartplayer) {
         const tr = document.createElement("tr");
 
         tr.innerHTML = `
-        <th>${i + 1.}</th>
-        <td>${name}</td>
+        <th class = "pl-20 text-white">${i + 1}. </th>
+        <td class = text-white>${name}</td>
         `;
 
         tableBody.appendChild(tr);
+
+
     }
+
+
 }
+
+
+document.getElementById("clicked").addEventListener('click', function () {
+    alert("You can clicked only 5 cart");
+})
+
+
 
 
 
 
 function addToCart(element) {
 
+    element.style.backgroundColor = "tomato"
+    element.style.border = "none"
+    element.style.color = "white"
+    element.innerText = 'SELECTED'
+    element.setAttribute('disabled', true)
+
+
+
     const playerName = element.parentNode.parentNode.children[1].innerText;
-    // console.log(playerName);
+
 
     const playerObj = {
         playerName: playerName,
@@ -36,15 +59,52 @@ function addToCart(element) {
 
     cartArray.push(playerObj);
 
-
-    console.log(cartArray);
-
     console.log(cartArray.length);
 
     display(cartArray);
 }
 
 
+
+document.getElementById("per-player-calculate-ammount").addEventListener('click', function () {
+
+    const ammountField = document.getElementById('player-ammount');
+    const newPerPlayerAmount = ammountField.value;
+
+    const PlayerAmount = parseFloat(newPerPlayerAmount);
+
+    const ammountTotalElement = document.getElementById("per-player-total");
+    const ammountTotal = ammountTotalElement.innerText;
+    ammountTotalElement.innerText = newPerPlayerAmount;
+
+})
+
+
+document.getElementById("calculate-total-ammount").addEventListener('click', function () {
+
+    const ammountTotalElement = document.getElementById("per-player-total");
+    const ammountTotal = ammountTotalElement.innerText;
+    const perAmmount = parseFloat(ammountTotal);
+
+
+    const ammountField = document.getElementById('manager-ammount');
+    const previousPerManagerAmount = ammountField.value;
+    const managerAmount = parseFloat(previousPerManagerAmount);
+
+    const coachAmmountField = document.getElementById('coach-ammount');
+    const perPlayerAmount = coachAmmountField.value;
+    const coachAmmount = parseFloat(perPlayerAmount);
+
+    const fullTotalAmmount = perAmmount + managerAmount + coachAmmount;
+
+
+    const coachAmmountTotalElement = document.getElementById('per-total-ammount');
+    coachAmmountTotalElement.innerText = fullTotalAmmount;
+
+    // console.log(fullTotalAmmount);
+
+
+})
 
 
 
